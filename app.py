b5120ca1f44481cc5f31e62ea576f03f7b529ece
hari <hariprasadkool@gmail.com>
2024-07-20 11:06:34 +0530

    added contact api, employee api, bakery api, ingredients api

--- app.py
+++ app.py
@@ -54,9 +54,45 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(100), nullable=False)
+    username=db.Column(db.String(100), unique=True, nullable=False)
     email=db.Column(db.String(100), unique=True, nullable=False)
     password=db.Column(db.String(100), nullable=False)
     mobile=db.Column(db.BigInteger)
+    
+# Define the Bakery class
+class Bakery(db.Model):
+    __tablename__ = 'bakery'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100))
+    price=db.Column(db.String(100))
+    imageUrl=db.Column(db.String(100))
+    quantity=db.Column(db.String(100))
+    
+# Define the Ingredients class
+class Ingredients(db.Model):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100))
+    quantity=db.Column(db.String(100))
+    purpose=db.Column(db.String(100))
+    orderItem=db.Column(db.Boolean)
+    
+# Define the Employees class
+class Employees(db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100))
+    
+# Define the Employees class
+class Contactus(db.Model):
+    __tablename__ = 'contactus'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100))
+    email=db.Column(db.String(100))
+    phone=db.Column(db.BigInteger)
+    message=db.Column(db.String(100))
+    
+
     
 # Set up the database engine
 engine = create_engine(f'mysql+pymysql://{db_username}:{db_password}@{host_address}:{port}/{db_name}')
@@ -80,11 +116,12 @@ def signup():
     mobile = data.get('mobile')
     email = data.get('email')
     password = data.get('password')
+    username = data.get('username')
     
-    if not name or not mobile or not email or not password:
+    if not name or not mobile or not email or not password or not username:
         return jsonify({"error": "Missing required fields"}), 400
     
-    new_user = User(name=name, mobile=mobile, email=email, password=password)
+    new_user = User(name=name, mobile=mobile, email=email, password=password,username=username)
     
     try:
         db.session.add(new_user)
@@ -98,14 +135,15 @@ def signup():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    
+    username = data.get('username')
     email = data.get('email')  # Can be mobile or email
     password = data.get('password')
     
     if not email or not password:
         return jsonify({"error": "Missing required fields"}), 400
 
-    # Find the user by mobile or email
+    # Find the user by username or email
+    # user = User.query.filter(User.username == username).first()
     user = User.query.filter(User.email == email).first()
     
     if user and (user.password == password):
@@ -121,6 +159,130 @@ def logout():
     session.pop('user_id', None)
     session.pop('user_name', None)
     return jsonify({"message": "Logout successful"}), 200
+
+# Define the route for the POST request to add all bakery
+@app.route('/bakery', methods=['POST'])
+def add_bakery():
+    data = request.get_json()
+    for bakery in data:
+        new_bakery = Bakery(
+            name=bakery['name'],
+            price=bakery['price'],
+            imageUrl=bakery['imageUrl'],
+            quantity=bakery['quantity']
+        )
+        db.session.add(new_bakery)
+    db.session.commit()
+    return jsonify({"message": "Bakery added successfully!"}), 201
+
+# Define the route for the GET request to fetch all bakery
+@app.route('/bakery', methods=['GET'])
+def get_cakes():
+    bakerys = Bakery.query.all()
+    results = [
+        {
+            "id": bakery.id,
+            "name": bakery.name,
+            "price": bakery.price,
+            "imageUrl": bakery.imageUrl,
+            "quantity": bakery.quantity
+        } for bakery in bakerys]
+
+    return jsonify(results), 200
+
+
+# Define the route for the POST request to add all ingredients
+@app.route('/ingredients', methods=['POST'])
+def add_ingredients():
+    data = request.get_json()
+    for ingredients in data:
+        new_ingredients = Ingredients(
+            name=ingredients['name'],
+            purpose=ingredients['purpose'],
+            orderItem=ingredients['orderItem'],
+            quantity=ingredients['quantity']
+        )
+        db.session.add(new_ingredients)
+    db.session.commit()
+    return jsonify({"message": "ingredients added successfully!"}), 201
+
+# Define the route for the GET request to fetch all ingredients
+@app.route('/ingredients', methods=['GET'])
+def get_ingredients():
+    ingredients = Ingredients.query.all()
+    results = [
+        {
+            "id": ingredient.id,
+            "name": ingredient.name,
+            "purpose": ingredient.purpose,
+            "orderItem": ingredient.orderItem,
+            "quantity": ingredient.quantity
+        } for ingredient in ingredients]
+
+    return jsonify(results), 200
+
+# Define the route for the POST request to add all Employees 
+@app.route('/employees', methods=['POST'])
+def add_employees():
+    data = request.get_json()
+    for employees in data:
+        new_employees = Employees(
+            name=employees['name'],
+        )
+        db.session.add(new_employees)
+    db.session.commit()
+    return jsonify({"message": "Employees added successfully!"}), 201
+
+# Define the route for the GET request to fetch all Employees 
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    employees = Employees.query.all()
+    results = [
+        {
+            "id": employee.id,
+            "name": employee.name
+        } for employee in employees]
+
+    return jsonify(results), 200
+
+
+# Define the route for the POST request to add all Contact Us 
+@app.route('/contactus', methods=['POST'])
+def add_contactus():
+    data = request.get_json()
+    
+    name = data.get('name')
+    phone = data.get('phone')
+    email = data.get('email')
+    message = data.get('message')
+
+    
+    new_contact = Contactus(name=name, phone=phone, email=email, message=message)
+    
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"error": "Error while saving contact"}), 409
+    
+    return jsonify({"message": "Contact created successfully"}), 201
+
+# Define the route for the GET request to fetch all Contact Us 
+@app.route('/contactus', methods=['GET'])
+def get_contactus():
+    contactus = Contactus.query.all()
+    results = [
+        {
+            "id": contact.id,
+            "name": contact.name,
+            "email": contact.email,
+            "phone": contact.phone,
+            "message": contact.message
+        } for contact in contactus]
+
+    return jsonify(results), 200
+
 
 
 ####################################################### main driver function#########################################################################################################
